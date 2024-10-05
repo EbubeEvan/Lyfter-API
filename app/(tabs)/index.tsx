39a844f66fa6c6a55596a016a2ad9a82873e4eda
Andreas Billy Sutandi <andreas.sutandi@atibusinessgroup.com>
2024-10-05 21:00:47 +0700

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,8 +4,18 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useEffect } from 'react';
 
 export default function HomeScreen() {
+
+  useEffect(() => {
+    const test = async() => {
+      const response = await fetch('/hello');
+      const data = await response.json();
+      alert('Hello ' + data.hello);
+    }
+    test()
+  },[])
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
